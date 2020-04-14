0d5e0017568f06fa600368ce81b09adaf0ebd8a4
Riyaz <52329974+riyazsonu7@users.noreply.github.com>
2020-04-14 19:36:06 +0530

    Update CreateTextfile.py

--- CreateTextfile.py
+++ CreateTextfile.py
@@ -1,4 +1,26 @@
 fh = open('example.txt','w')
-fh.write('the file is created and text is added to the file')
+
+for i in range(1,11):
+    print(fh.write("you are reading Line : %d \n"%i))
+
 fh.close()
 
+fh = open('example.txt','r')
+# print(fh.read(3))
+# print(fh.read(10))
+#print(fh.readline())
+#print(fh.readlines()) #convert lines intolist
+# print(fh.readlines()[0])
+#print(len(fh.readlines()[0]))
+
+# for line in fh:
+#     print(line.split(" "))
+
+#now read the file and copy to the next file
+
+kt =open('test.txt','w')
+kt.write(fh.readlines()[0])
+
+kt.close()
+
+fh.close()
